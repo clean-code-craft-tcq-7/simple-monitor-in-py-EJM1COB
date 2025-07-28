e0a25bb507e3b8f40be26d9fc27d3d1ab2c041bc
Ranjith <ejm1cob@bosch.com>
2025-07-28 17:29:48 +0530

    remove duplicate

--- monitor.py
+++ monitor.py
@@ -2,37 +2,23 @@
 from time import sleep
 import sys
 
-
 def vitals_ok(temperature, pulseRate, spo2):
   if temperature > 102 or temperature < 95:
-    print('sample')
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
+   vital_alert('Temperature critical!')
     return False
   elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
+    vital_alert('Pulse Rate is out of range!')
     return False
   elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
+    vital_alert('Oxygen Saturation out of range!')
+    return False
+  return True
+  def vital_alert(message)
+  print(message)
+  for i in range(6):
       print('\r* ', end='')
       sys.stdout.flush()
       sleep(1)
       print('\r *', end='')
       sys.stdout.flush()
       sleep(1)
-    return False
-  return True
