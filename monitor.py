1ba8b5e58c0b7ddcd7fdb34dab992ace74967727
Ranjith <ejm1cob@bosch.com>
2025-07-28 17:32:49 +0530

    changes

--- monitor.py
+++ monitor.py
@@ -2,6 +2,16 @@
 from time import sleep
 import sys
 
+def vital_alert(message)
+  print(message)
+  for i in range(6):
+      print('\r* ', end='')
+      sys.stdout.flush()
+      sleep(1)
+      print('\r *', end='')
+      sys.stdout.flush()
+      sleep(1)
+
 def vitals_ok(temperature, pulseRate, spo2):
   if temperature > 102 or temperature < 95:
    vital_alert('Temperature critical!')
@@ -13,12 +23,4 @@ def vitals_ok(temperature, pulseRate, spo2):
     vital_alert('Oxygen Saturation out of range!')
     return False
   return True
-  def vital_alert(message)
-  print(message)
-  for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
+  
